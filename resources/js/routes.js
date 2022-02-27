44de9066d55221b9f35cc6c587023a99488fb5b4
Arun Kumar <kumar05.962013@gmail.com>
2022-02-27 18:05:14 +0530

    initial setup

---
+++ resources/js/routes.js
@@ -0,0 +1,13 @@
+import VueRouter from "vue-router";
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: ExampleComponent,
+    },
+];
+
+const router = new VueRouter({ mode: "history", routes });
+
+export default router;
